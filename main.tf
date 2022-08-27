de0a3b83930f32f4f68c4cfa9872c5af48e4b18e
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-27 16:12:03 +1000

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,31 +1,348 @@
-resource "aws_autoscaling_group" "hatest" {
-  name                      = var.repave_strategy == "bluegreen" ? "${var.aws_launch_configuration_name}-asg" : "rolling-asg"
-  vpc_zone_identifier       = var.aws_subnet_ids
-  launch_configuration      = var.aws_launch_configuration_name
-  desired_capacity          = 3
-  min_size                  = 3
-  max_size                  = 5
-  health_check_grace_period = 300
-  health_check_type         = "ELB"
-  load_balancers            = [var.aws_elb_name]
-  force_delete              = true
+data "aws_partition" "current" {}
+data "aws_default_tags" "current" {}
 
-  tag {
-    key                 = "environment"
-    value               = var.app_env
-    propagate_at_launch = true
+locals {
+  create = var.create 
+
+  launch_template_name    = coalesce(var.launch_template_name, var.name)
+  launch_template         = var.create_launch_template ? aws_launch_template.this[0].name : var.launch_template
+  launch_template_version = var.create_launch_template && var.launch_template_version == null ? aws_launch_template.this[0].latest_version : var.launch_template_version
+
+  asg_tags = merge(
+    data.aws_default_tags.current.tags,
+    var.tags,
+    { "Name" = coalesce(var.instance_name, var.name) },
+    var.autoscaling_group_tags,
+  )
+}
+
+locals {
+  iam_instance_profile_arn  = var.create_iam_instance_profile ? aws_iam_instance_profile.this[0].arn : var.iam_instance_profile_arn
+  iam_instance_profile_name = !var.create_iam_instance_profile && var.iam_instance_profile_arn == null ? var.iam_instance_profile_name : null
+}
+
+resource "aws_autoscaling_group" "this" {
+  count = local.create && !var.ignore_desired_capacity_changes ? 1 : 0
+
+  name        = var.use_name_prefix ? null : var.name
+  name_prefix = var.use_name_prefix ? "${var.name}-" : null
+
+  dynamic "launch_template" {
+    for_each = var.use_mixed_instances_policy ? [] : [1]
+
+    content {
+      name    = local.launch_template
+      version = local.launch_template_version
+    }
   }
 
-  tag {
-    key                 = "appname"
-    value               = var.app_name
-    propagate_at_launch = true
+  availability_zones  = var.availability_zones
+  vpc_zone_identifier = var.vpc_zone_identifier
+
+  min_size                  = var.min_size
+  max_size                  = var.max_size
+  desired_capacity          = var.desired_capacity
+  capacity_rebalance        = var.capacity_rebalance
+  min_elb_capacity          = var.min_elb_capacity
+  wait_for_elb_capacity     = var.wait_for_elb_capacity
+  wait_for_capacity_timeout = var.wait_for_capacity_timeout
+  default_cooldown          = var.default_cooldown
+  protect_from_scale_in     = var.protect_from_scale_in
+
+  load_balancers            = var.load_balancers
+  target_group_arns         = var.target_group_arns
+  placement_group           = var.placement_group
+  health_check_type         = var.health_check_type
+  health_check_grace_period = var.health_check_grace_period
+
+  force_delete          = var.force_delete
+  termination_policies  = var.termination_policies
+  suspended_processes   = var.suspended_processes
+  max_instance_lifetime = var.max_instance_lifetime
+
+  enabled_metrics         = var.enabled_metrics
+  metrics_granularity     = var.metrics_granularity
+  service_linked_role_arn = var.service_linked_role_arn
+
+  dynamic "initial_lifecycle_hook" {
+    for_each = var.initial_lifecycle_hooks
+    content {
+      name                    = initial_lifecycle_hook.value.name
+      default_result          = try(initial_lifecycle_hook.value.default_result, null)
+      heartbeat_timeout       = try(initial_lifecycle_hook.value.heartbeat_timeout, null)
+      lifecycle_transition    = initial_lifecycle_hook.value.lifecycle_transition
+      notification_metadata   = try(initial_lifecycle_hook.value.notification_metadata, null)
+      notification_target_arn = try(initial_lifecycle_hook.value.notification_target_arn, null)
+      role_arn                = try(initial_lifecycle_hook.value.role_arn, null)
+    }
   }
 
-  tag {
-    key                 = "app_id"
-    value               = var.app_id
-    propagate_at_launch = true
+  dynamic "instance_refresh" {
+    for_each = length(var.instance_refresh) > 0 ? [var.instance_refresh] : []
+    content {
+      strategy = instance_refresh.value.strategy
+      triggers = try(instance_refresh.value.triggers, null)
+
+      dynamic "preferences" {
+        for_each = try([instance_refresh.value.preferences], [])
+        content {
+          checkpoint_delay       = try(preferences.value.checkpoint_delay, null)
+          checkpoint_percentages = try(preferences.value.checkpoint_percentages, null)
+          instance_warmup        = try(preferences.value.instance_warmup, null)
+          min_healthy_percentage = try(preferences.value.min_healthy_percentage, null)
+        }
+      }
+    }
   }
 
+  dynamic "mixed_instances_policy" {
+    for_each = var.use_mixed_instances_policy ? [var.mixed_instances_policy] : []
+    content {
+      dynamic "instances_distribution" {
+        for_each = try([mixed_instances_policy.value.instances_distribution], [])
+        content {
+          on_demand_allocation_strategy            = try(instances_distribution.value.on_demand_allocation_strategy, null)
+          on_demand_base_capacity                  = try(instances_distribution.value.on_demand_base_capacity, null)
+          on_demand_percentage_above_base_capacity = try(instances_distribution.value.on_demand_percentage_above_base_capacity, null)
+          spot_allocation_strategy                 = try(instances_distribution.value.spot_allocation_strategy, null)
+          spot_instance_pools                      = try(instances_distribution.value.spot_instance_pools, null)
+          spot_max_price                           = try(instances_distribution.value.spot_max_price, null)
+        }
+      }
+
+      launch_template {
+        launch_template_specification {
+          launch_template_name = local.launch_template
+          version              = local.launch_template_version
+        }
+
+        dynamic "override" {
+          for_each = try(mixed_instances_policy.value.override, [])
+          content {
+            instance_type     = try(override.value.instance_type, null)
+            weighted_capacity = try(override.value.weighted_capacity, null)
+
+            dynamic "launch_template_specification" {
+              for_each = try([override.value.launch_template_specification], [])
+              content {
+                launch_template_id = try(launch_template_specification.value.launch_template_id, null)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  dynamic "warm_pool" {
+    for_each = length(var.warm_pool) > 0 ? [var.warm_pool] : []
+    content {
+      pool_state                  = try(warm_pool.value.pool_state, null)
+      min_size                    = try(warm_pool.value.min_size, null)
+      max_group_prepared_capacity = try(warm_pool.value.max_group_prepared_capacity, null)
+
+      dynamic "instance_reuse_policy" {
+        for_each = try([warm_pool.value.instance_reuse_policy], [])
+        content {
+          reuse_on_scale_in = try(instance_reuse_policy.value.reuse_on_scale_in, null)
+        }
+      }
+    }
+  }
+
+  timeouts {
+    delete = var.delete_timeout
+  }
+
+  dynamic "tag" {
+    for_each = local.asg_tags
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_policy" "this" {
+  for_each = { for k, v in var.scaling_policies : k => v if local.create && var.create_scaling_policy }
+
+  name                   = try(each.value.name, each.key)
+  autoscaling_group_name = var.ignore_desired_capacity_changes ? aws_autoscaling_group.idc[0].name : aws_autoscaling_group.this[0].name
+
+  adjustment_type           = try(each.value.adjustment_type, null)
+  policy_type               = try(each.value.policy_type, null)
+  estimated_instance_warmup = try(each.value.estimated_instance_warmup, null)
+  cooldown                  = try(each.value.cooldown, null)
+  min_adjustment_magnitude  = try(each.value.min_adjustment_magnitude, null)
+  metric_aggregation_type   = try(each.value.metric_aggregation_type, null)
+  scaling_adjustment        = try(each.value.scaling_adjustment, null)
+
+  dynamic "step_adjustment" {
+    for_each = try([each.value.step_adjustment], [])
+    content {
+      scaling_adjustment          = step_adjustment.value.scaling_adjustment
+      metric_interval_lower_bound = try(step_adjustment.value.metric_interval_lower_bound, null)
+      metric_interval_upper_bound = try(step_adjustment.value.metric_interval_upper_bound, null)
+    }
+  }
+
+  dynamic "target_tracking_configuration" {
+    for_each = try([each.value.target_tracking_configuration], [])
+    content {
+      target_value     = target_tracking_configuration.value.target_value
+      disable_scale_in = try(target_tracking_configuration.value.disable_scale_in, null)
+
+      dynamic "predefined_metric_specification" {
+        for_each = try([target_tracking_configuration.value.predefined_metric_specification], [])
+        content {
+          predefined_metric_type = predefined_metric_specification.value.predefined_metric_type
+          resource_label         = try(predefined_metric_specification.value.resource_label, null)
+        }
+      }
+
+      dynamic "customized_metric_specification" {
+        for_each = try([target_tracking_configuration.value.customized_metric_specification], [])
+        content {
+
+          dynamic "metric_dimension" {
+            for_each = try([customized_metric_specification.value.metric_dimension], [])
+            content {
+              name  = try(metric_dimension.value.name, null)
+              value = try(metric_dimension.value.value, null)
+            }
+          }
+
+          metric_name = customized_metric_specification.value.metric_name
+          namespace   = customized_metric_specification.value.namespace
+          statistic   = customized_metric_specification.value.statistic
+          unit        = try(customized_metric_specification.value.unit, null)
+        }
+      }
+    }
+  }
+
+  dynamic "predictive_scaling_configuration" {
+    for_each = try([each.value.predictive_scaling_configuration], [])
+    content {
+      max_capacity_breach_behavior = try(predictive_scaling_configuration.value.max_capacity_breach_behavior, null)
+      max_capacity_buffer          = try(predictive_scaling_configuration.value.max_capacity_buffer, null)
+      mode                         = try(predictive_scaling_configuration.value.mode, null)
+      scheduling_buffer_time       = try(predictive_scaling_configuration.value.scheduling_buffer_time, null)
+
+      dynamic "metric_specification" {
+        for_each = try([predictive_scaling_configuration.value.metric_specification], [])
+        content {
+          target_value = metric_specification.value.target_value
+
+          dynamic "predefined_load_metric_specification" {
+            for_each = try([metric_specification.value.predefined_load_metric_specification], [])
+            content {
+              predefined_metric_type = predefined_load_metric_specification.value.predefined_metric_type
+              resource_label         = predefined_load_metric_specification.value.resource_label
+            }
+          }
+
+          dynamic "predefined_metric_pair_specification" {
+            for_each = try([metric_specification.value.predefined_metric_pair_specification], [])
+            content {
+              predefined_metric_type = predefined_metric_pair_specification.value.predefined_metric_type
+              resource_label         = predefined_metric_pair_specification.value.resource_label
+            }
+          }
+
+          dynamic "predefined_scaling_metric_specification" {
+            for_each = try([metric_specification.value.predefined_scaling_metric_specification], [])
+            content {
+              predefined_metric_type = predefined_scaling_metric_specification.value.predefined_metric_type
+              resource_label         = predefined_scaling_metric_specification.value.resource_label
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+################################################################################
+# IAM Role / Instance Profile
+################################################################################
+
+locals {
+  internal_iam_instance_profile_name = try(coalesce(var.iam_instance_profile_name, var.iam_role_name), "")
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  count = local.create && var.create_iam_instance_profile ? 1 : 0
+
+  statement {
+    sid     = "EC2AssumeRole"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.${data.aws_partition.current.dns_suffix}"]
+    }
+  }
+}
+
+resource "aws_iam_role" "this" {
+  count = local.create && var.create_iam_instance_profile ? 1 : 0
+
+  name        = var.iam_role_use_name_prefix ? null : local.internal_iam_instance_profile_name
+  name_prefix = var.iam_role_use_name_prefix ? "${local.internal_iam_instance_profile_name}-" : null
+  path        = var.iam_role_path
+  description = var.iam_role_description
+
+  assume_role_policy    = data.aws_iam_policy_document.assume_role_policy[0].json
+  permissions_boundary  = var.iam_role_permissions_boundary
+  force_detach_policies = true
+
+  tags = merge(var.tags, var.iam_role_tags)
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  for_each = { for k, v in var.iam_role_policies : k => v if var.create && var.create_iam_instance_profile }
+
+  policy_arn = each.value
+  role       = aws_iam_role.this[0].name
+}
+
+resource "aws_iam_instance_profile" "this" {
+  count = local.create && var.create_iam_instance_profile ? 1 : 0
+
+  role = aws_iam_role.this[0].name
+
+  name        = var.iam_role_use_name_prefix ? null : var.iam_role_name
+  name_prefix = var.iam_role_use_name_prefix ? "${var.iam_role_name}-" : null
+  path        = var.iam_role_path
+
+  tags = merge(var.tags, var.iam_role_tags)
+}
+
+resource "aws_sns_topic" "asg_sns_topic" {
+  name = "${var.layer}-sns-topic"
+}
+
+## SNS - Subscription
+resource "aws_sns_topic_subscription" "sns_topic_subscription" {
+  topic_arn = aws_sns_topic.asg_sns_topic.arn
+  protocol  = "email"
+  endpoint  = var.snsemail
+}
+
+## Create Autoscaling Notification Resource
+resource "aws_autoscaling_notification" "asg_notifications" {
+  group_names = [aws_autoscaling_group.this.id]
+  notifications = [
+    "autoscaling:EC2_INSTANCE_LAUNCH",
+    "autoscaling:EC2_INSTANCE_TERMINATE",
+    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
+    "autoscaling:EC2_INSTANCE_TERMINATE_ERROR",
+  ]
+  topic_arn = aws_sns_topic.asg_sns_topic.arn 
 }
